acc465ece2379cf109290d4c2ecde0a560bac519
Colin Regan <colin.regan@fdmgroup.com>
2025-02-14 12:05:39 +0000

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import * as tslib from 'tslib';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +6,7 @@ import * as tslib from 'tslib';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  images = ['assets/love1.jpg', 'assets/love2.jpg', 'assets/love3.jpg'];
+  images = ['./assets/love1.jpg', './assets/love2.jpg', './assets/love3.jpg', './assets/love4.jpg', './assets/love5.jpg', './assets/love6.jpg', './assets/love7.jpg'];
   currentIndex = 0;
   currentImage = this.images[this.currentIndex];
 
@@ -16,8 +15,17 @@ export class AppComponent {
     this.currentImage = this.images[this.currentIndex];
   }
   
+  // playMusic() {
+  //   const audio: HTMLAudioElement = document.querySelector('#audioPlayer')!;
+  //   audio.play();
+  // }
   playMusic() {
-    const audio: HTMLAudioElement = document.querySelector('#audioPlayer')!;
-    audio.play();
+    const audio = document.querySelector('audio');
+    if (audio) {
+      audio.play().catch(error => console.error('Error playing audio:', error));
+    } else {
+      console.error('Audio element not found');
+    }
   }
+  
 }
